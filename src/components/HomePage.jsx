2f8d578b862e84f80b90a167697a42d6236bb620
taynguyen3110 <taynguyen3110@gmail.com>
2024-08-29 21:44:45 +1000

    project init

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <div className='bg-white'>
+      <div className='container'>
+        <h1 className='text-4xl font-bold text-center mt-10'>Welcome to the Home Page</h1>
+      </div>
+    </div>
+  )
+}

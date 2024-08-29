7b15b8f83670661eb02e6e68294b6562add7f4cf
taynguyen3110 <taynguyen3110@gmail.com>
2024-08-29 22:28:16 +1000

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import logo from '../../public/assets/images/Logomark.png'
+import chev from '../../public/assets/images/chevron-down.png'
 
 export const Header = () => {
     return (
-        <div className='container flex flex-row'>
-            <div>
-                <img src={logo} alt="" />
-                <h1>Untitled UI</h1>
+        <div className='container flex justify-between'>
+            <div className=' flex gap-10'>
+                <div className='flex items-center'>
+                    <img src={logo} alt="" />
+                    <h1>Untitled UI</h1>
+                </div>
+                <div className='flex items-center gap-8'>
+                    <a href="">
+                        <div className=''>Home</div>
+                    </a>
+                    <a href="" className='flex'>
+                        <div>Products</div>
+                        <img src={chev} alt="" />
+                    </a>
+                    <a href="" className='flex'>
+                        <div>Resources</div>
+                        <img src={chev} alt="" />
+                    </a>
+                    <a href="">
+                        <div>Pricing</div>
+                    </a>
+                </div>
             </div>
-            <div className='flex'>
-                <a href="">
-                    <div className='text-4xl'>Home</div>
-                </a>
-                <a href="">
-                    <div>Products</div>
-                </a>
-                <a href="">
-                    <div>Resources</div>
-                </a>
-                <a href="">
-                    <div>Pricing</div>
-                </a>
-            </div>
-            <div className='flex'>
-                <div>Log in</div>
-                <button>Sign up</button>
+            <div className='flex items-center gap-8'>
+                <div className='text-nowrap'>Log in</div>
+                <button className='text-nowrap rounded-lg px-3 py-1 bg-purple-600 text-white'>Sign up</button>
             </div>
         </div>
+
     )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Figures from "./Figures";
+import { Header } from "./Header";
 
 export const HomePage = () => {
   return (
     <div className='bg-white'>
       <div className='container'>
+        <Header />
         <h1 className='text-4xl font-bold text-center mt-10'>Welcome to the Home Page</h1>
       </div>
     </div>

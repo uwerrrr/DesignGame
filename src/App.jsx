db5985ad89d0f77576b33d54fd9f506b1f32ebf2
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2024-08-29 22:05:15 +1000

    add figures component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { HomePage } from './components/HomePage'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { HomePage } from "./components/HomePage";
 
 function App() {
-
   return (
     <>
       <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
+import Figures from "./Figures";
 
 export const HomePage = () => {
   return (
-    <div className='bg-white'>
-      <div className='container'>
-        <h1 className='text-4xl font-bold text-center mt-10'>Welcome to the Home Page</h1>
-      </div>
+    <div className="bg-white">
+      abc
+      <Figures />
     </div>
-  )
-}
+  );
+};
